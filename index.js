b9f46dac7dc2975ed37f22fb67bebf41a7707102
KievL <kiev.sider@hotmail.com>
2023-04-17 11:13:21 -0300

    bugs fixed

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ $("#form-contact").submit(function(e){
     e.preventDefault();
 });
 
+const btnWho = document.getElementById("menu-item-who");
+const btnServices = document.getElementById("menu-item-services");
+const btnContact = document.getElementById("menu-item-contact");
+
 const btnNavBar = document.getElementById("btn-mobile");
 const btnCloseNavBar = document.getElementById("btn-mobile-close");
 
@@ -26,6 +30,9 @@ const exitMenuMobile = () => {
     console.log("test")
 }
 
+btnWho.addEventListener('click', exitMenuMobile);
+btnServices.addEventListener('click', exitMenuMobile);
+btnContact.addEventListener('click', exitMenuMobile);
 
 btnNavBar.addEventListener('click', enterMenuMobile);
 btnCloseNavBar.addEventListener('click', exitMenuMobile);
